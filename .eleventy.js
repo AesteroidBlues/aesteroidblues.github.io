0cac867ec2a831ba87164f5c68768fbaa905834a
Alex Ferbrache <Alex.Ferbrache@gmail.com>
2023-06-29 11:45:00 -0700

    Blog, jobs

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,8 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy('src/img')
+    eleventyConfig.addPassthroughCopy('src/img');
+    eleventyConfig.addPassthroughCopy('src/blog/**/*.png');
 
 
   const {
