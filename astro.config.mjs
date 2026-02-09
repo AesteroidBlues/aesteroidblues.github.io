6e9fe160e209364c7ef51012ac75e2dfd5065bfe
Alex Ferbrache <Alex.Ferbrache@gmail.com>
2026-02-08 16:59:44 -0800

    Nah nevermind

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://aesteroidblues.github.io',
-	base: '/alexmakes.games',
 	integrations: [mdx(), sitemap()],
 	vite: { plugins: [tailwindcss()], },
 });
